f98fbe3cfe6b37646223de84d0a65243da265497
sonle2003 <levietson2017@gmail.com>
2023-03-01 16:09:49 +0700

    new

--- components/layouts/components/Header/index.tsx
+++ components/layouts/components/Header/index.tsx
@@ -7,6 +7,8 @@ import {AiOutlineCheckCircle} from 'react-icons/ai'
 import classNames from 'classnames/bind'
 import styles from './Header.module.scss'
 
+
+
 const cx = classNames.bind(styles)
 const logo1="https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/329030659_1417820318956584_1663568495640829760_n.png?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=uv-zJll1gCoAX_bx4Gu&tn=DoiO9q6o7GFj9poR&_nc_ht=scontent.fhan14-2.fna&oh=03_AdSK3KWYegb7b-YGv04m2_dyjptVYPTBe9SJnvy-FJgMiQ&oe=64255146";
 export default function Header() {
@@ -15,9 +17,15 @@ export default function Header() {
     <Logo className={cx('h-[60px] p-[7px] ')} logo={logo1} name={'Smart device'}/>
     <Search/>
     <div  className={cx('flex items-center')}>
-      <HeaderItem icon={<BsTelephoneFill/>} content="Bán hàng" type="link" />
-      <HeaderItem icon={<AiOutlineCheckCircle/>} content="Bảo hành" type=""/>
-      <HeaderItem icon={<BsCartDash/>} content="Giỏ hàng" type=""/>
+      <HeaderItem link={''} type='about' quanity={null} icon={<BsTelephoneFill width={'20px'} height={'20px'}/>} content={<>
+        <p className={cx('text-[12px]')}>Bán hàng</p>
+        <p className={cx('text-[16px] leading-none')}>Online</p>
+      </>}/>
+      <HeaderItem link={''} type='about' quanity={null} icon={<AiOutlineCheckCircle/>} content={<>
+        <p className={cx('text-[12px]')}>Tra cứu</p>
+        <p className={cx('text-[16px] leading-none')}>Bảo hành</p>
+      </>}/>
+      <HeaderItem link={'/'} type='link' quanity={0} icon={<BsCartDash/>} content="Giỏ hàng"/>
     </div>
     </div>
   )
