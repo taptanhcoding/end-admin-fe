c1b93bff12b4ad5a0dff42f0e2c4132a310d9278
sonle2003 <levietson2017@gmail.com>
2023-03-08 00:18:17 +0700

    'check'

--- pages/san-pham/[slugProduct].tsx
+++ pages/san-pham/[slugProduct].tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import ProductIteam from '../../components/ProductIteam'
+import classNames from "classnames/bind";
+import styles from "../../components/ProductIteam/ProductItem.module.scss";
 
+
+interface props{
+  
+}
 export default function ProductDetail() {
   return (
-    <div>san-pham-[slugProduct]</div>
+    <div>
+      <div>
+      tiển ngu
+      </div>
+    </div>
   )
 }
